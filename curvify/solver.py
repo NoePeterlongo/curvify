098c4e79c0ea5fc5be117166f3af72e9e092046f
NoePeterlongo <now@y.com>
2025-11-02 07:36:30 +0100

    feat: build report

--- curvify/solver.py
+++ curvify/solver.py
@@ -1,6 +1,7 @@
 from scipy.optimize import curve_fit
 import numpy as np
 import re
+import copy
 from dataclasses import dataclass
 
 from .models_library import fourier
@@ -18,6 +19,7 @@ class Param:
 class Solver:
     def __init__(self):
         self.is_valid_ = False
+        self.report: str = ""
 
     def update_model(self, function_str: str):
         # TODO checks : nb of params, execution ?
@@ -34,6 +36,7 @@ class Solver:
             self.is_valid_ = False
             return
         self.model = model
+        self.model_str = function_str
         self.is_valid_ = True
 
         #TODO keep existing parameters ?
@@ -57,7 +60,6 @@ class Solver:
         return vectorized_model(x)
 
     def fit(self, x_data: np.ndarray, y_data: np.ndarray) -> tuple[bool, dict]:
-        # TODO: check the nb of points vs the number of parmaeters
         p0 = [param.value for param in self.params]
         upper_bounds = [p.value+1e-15 if p.locked else p.max_value for p in self.params]
         lower_bounds = [p.value if p.locked else p.min_value for p in self.params]
@@ -67,6 +69,10 @@ class Solver:
         except (RuntimeError, TypeError) as e:
             print(f"Error during fitting: {e}")
             return False, {}
+        
+        # Store initial params for report
+        initial_params = copy.deepcopy(self.params)
+
         for i, param in enumerate(self.params):
             param.value = float(params[i])
             param.error = float(error[i])
@@ -81,8 +87,39 @@ class Solver:
         results["R2"] = r_squared
         rmse = np.sqrt(np.mean((y_data - y_pred) ** 2))
         results["RMSE"] = rmse
+
+        self.build_report(x_data, y_data, self.model_str, initial_params, results)
         
         return True, results
+    
+    def build_report(
+            self, 
+            x_data: np.ndarray, 
+            y_data: np.ndarray, 
+            model_str: str, 
+            initial_params: list[Param], 
+            results: dict
+        ):
+
+        filled_model = model_str.split(":")[1].strip()
+        for name, value in results["params"].items():
+            filled_model = re.sub(fr"\b({name})\b", f"{value:.5e}", filled_model)
+
+        self.report = ""
+        self.report += "Data :\n"
+        self.report += f"\tx_data = [{', '.join(map(lambda x: f"{x:.4e}", x_data))}]\n"
+        self.report += f"\ty_data = [{', '.join(map(lambda x: f"{x:.4e}", y_data))}]\n"
+        self.report += "\n"
+        self.report += "Model :\n"
+        self.report += f"\ty = {model_str.split(':')[1]}\n"
+        init_values = {p.name: p.value for p in initial_params}
+        self.report += f"\tinitial_parameters = {init_values}\n"
+        self.report += f"\t locked_parameters = [{', '.join([p.name for p in initial_params if p.locked])}]\n"
+        self.report += "\n"
+        self.report += "Results :\n"
+        self.report += f"\tmodel: y = {filled_model}\n"
+        for key, value in results.items():
+            self.report += f"\t{key} = {value}\n"
 
 
 if __name__ == "__main__":
